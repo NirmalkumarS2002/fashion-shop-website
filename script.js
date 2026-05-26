787870b77293225f5b40bb050539d5e81ba358bb
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-05-26 11:28:23 +0530

    deals section started

--- script.js
+++ script.js
@@ -45,3 +45,65 @@ right.addEventListener("click", () => {
     }
     changeSlide()
 })
+
+// // showcase
+
+// let sbtn1=document.querySelector(".sbtn1")
+// let sbtn2=document.querySelector(".sbtn2")
+
+// let sbox1=document.querySelector(".sbox1")
+// let sbox2=document.querySelector(".sbox2")
+// let sbox3=document.querySelector(".sbox3")
+// let sbox4=document.querySelector(".sbox4")
+// let sbox5=document.querySelector(".sbox5")
+
+// sbtn1.addEventListener("click",()=>{
+//     sbox5.style.display="block"
+//      sbox1.style.display="none"
+// })
+
+// sbtn2.addEventListener("click",()=>{
+//     sbox5.style.display="none"
+//      sbox1.style.display="block"
+// })
+
+let showcase = document.querySelector(".showcase")
+
+let btn1 = document.querySelector(".sbtn1")
+let btn2 = document.querySelector(".sbtn2")
+
+let scrollAmount = 0
+
+
+btn2.addEventListener("click",()=>{
+
+    scrollAmount += 300
+
+    showcase.scrollTo({
+        left: scrollAmount,
+        behavior:"smooth"
+    })
+
+    btn2.classList.add("active")
+    btn1.classList.remove("active")
+
+})
+
+
+btn1.addEventListener("click",()=>{
+
+    scrollAmount -= 300
+
+    if(scrollAmount < 0){
+        scrollAmount = 0
+    }
+
+    showcase.scrollTo({
+        left: scrollAmount,
+        behavior:"smooth"
+    })
+
+    btn1.classList.add("active")
+    btn2.classList.remove("active")
+
+})
